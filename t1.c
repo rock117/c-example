ab705c1bb1d2ad50a7ab3d60e74be951ea4f4849
rhuang <rock.huang@morningstar.com>
2015-03-09 16:59:52 +0800

    add

---
+++ t1.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdio.h>
+pthread_t ntid;
+
+void printids(const char *s){
+	pid_t pid;
+	pthread_t tid;
+	pid = getpid();
+	tid = pthread_self();
+	printf("%s pid %u tid %u (0x%x) \n",  s, (unsigned int)pid, (unsigned int)tid, (unsigned int)tid);
+}
+
+void *thr_fn(void *arg){
+	printids("new thread: ");
+	return ((void *)0);
+}
+
+int main(void){
+	int err = pthread_create(&ntid, NULL, thr_fn, NULL);
+	printids("main thread: ");
+	sleep(1);
+	exit(0);
+}
